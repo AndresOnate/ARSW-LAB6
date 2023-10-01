3fcd701b8317dc7204a2d5ea253d35b29e1b1306
AndresOnate <andres.onate@mail.escuelaing.edu.co>
2023-09-30 21:45:28 -0500

    Parte 2 app.js

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,17 +1,37 @@
-apiBlueprint=(function(){
+var app = (function(){
 
-    var selectedAuthor = null;
-    var blueprintData = [];
+    var author = "";
+    var blueprints = [];
+
+    function setAuthorName(author) {
+        author = author;
+    }
+
+	function updateBlueprintTable() {
+        blueprints.map(function (blueprint) {
+            var newRow = "<tr><td>" + blueprint.name + "</td><td>" + blueprint.points.length + "</td></tr>";
+            $("#blueprint-table tbody").append(newRow);
+        });
+        var totalPoints = blueprints.reduce(function (accumulator, blueprint) {
+            return accumulator + blueprint.points.length;
+        }, 0);
+        $("#total-points").text("Total user points: " + totalPoints);
+    }
+
+	function getBlueprintsByAuthor() {
+        author = $("#author").val();
+		$("#blueprint-table tbody").empty();
+		$("#author-content").text(author + "'s blueprints: ");
+        apimock.getBlueprintsByAuthor(author, function (data) {
+            blueprints = data.map(function (blueprint) {
+                return { name: blueprint.name, points: blueprint.points };
+            });
+            updateBlueprintTable(); 
+        });
+    }
 
 	return {
-		setSelectedAuthor:function(authname){
-		    selectedAuthor = authname;
-		},
-		getSelectedAuthor:function(callback){
-			callback(
-				selectedAuthor
-			);
-		},
+	    getBlueprintsByAuthor: getBlueprintsByAuthor,
+        setAuthorName: setAuthorName
 	}
-
 })();
